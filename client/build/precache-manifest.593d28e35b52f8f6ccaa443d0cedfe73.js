cb4a68c480d3a01e2b8b8c711678ed7ba11db293
sarah-angel <sarangel333@gmail.com>
2020-03-27 13:16:34 +0300

    Procfile

---
+++ client/build/precache-manifest.593d28e35b52f8f6ccaa443d0cedfe73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f5d09e2f8932de891d456b59bdb1d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e55b915f369560ce7c17",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "55fa9bced837424e13fa",
+    "url": "/static/js/2.552ef72d.chunk.js"
+  },
+  {
+    "revision": "66fe890d6e603a726418b388d0eed6c0",
+    "url": "/static/js/2.552ef72d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55b915f369560ce7c17",
+    "url": "/static/js/main.04b9a56c.chunk.js"
+  },
+  {
+    "revision": "8e3468b942cbb363631b",
+    "url": "/static/js/runtime-main.173a09d2.js"
+  }
+]);
